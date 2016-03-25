c071ecff33eef6381b532908851faa4712e40ab3
vadim <v@dim.com>
2016-03-25 13:02:58 +0200

    new line to unix style

---
+++ sqlcmd/src/main/java/main/Main.java
@@ -0,0 +1,7 @@
+package main;
+
+/**
+ * Created by v.kalitsev on 3/25/2016.
+ */
+public class Main {
+}
---
+++ sqlcmd/src/test/java/main/MainTest.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by v.kalitsev on 3/25/2016.
+ */
+public class MainTest {
+
+    @Test
+    public void Init() {
+        assertEquals("test", "test");
+    }
+
+}
