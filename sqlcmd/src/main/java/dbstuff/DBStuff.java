d3ad45df0843faf6a8c560f5209347d33e257941
vadim <v@dim.com>
2016-04-08 21:24:29 +0300

    init

--- sqlcmd/src/main/java/dbstuff/DBStuff.java
+++ sqlcmd/src/main/java/dbstuff/DBStuff.java
@@ -1,7 +1,40 @@
 package dbstuff;
 
-/**
- * Created by vadim on 4/5/16.
- */
+import com.mysql.fabric.jdbc.FabricMySQLDriver;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBStuff {
+
+    public void connect(String DBURL, String DBUSER, String DBPASS) {
+        Connection connection;
+
+        try
+
+        {
+            Driver driver = new FabricMySQLDriver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+
+            if (!connection.isClosed()) {
+                System.out.println("connection to DB is open");
+            }
+
+            connection.close();
+
+            if (connection.isClosed()) {
+                System.out.println("connection to DB was closed");
+            }
+        } catch (
+                SQLException e
+                )
+
+        {
+            e.printStackTrace();
+        }
+
+    }
 }
