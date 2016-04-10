8754de2bc36dd30ca250164641a8b3104ee3798b
vadim <v@dim.com>
2016-04-10 12:15:16 +0300

    base help

--- sqlcmd/src/main/java/help/Help.java
+++ sqlcmd/src/main/java/help/Help.java
@@ -1,10 +1,18 @@
 package help;
 
+import dbstuff.DBStuff;
+
+import java.lang.reflect.Method;
+
 /**
  * Created by vadim on 4/5/16.
  */
 public class Help {
-    public void commandList() {
-
+    public static void commandList() {
+        Method[] declaredMethods = DBStuff.class.getDeclaredMethods();
+        System.out.println("Only next available");
+        for (Method m : declaredMethods) {
+            System.out.println("\t" + m.getName());
+        }
     }
 }
--- sqlcmd/src/main/java/main/Main.java
+++ sqlcmd/src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
 import dbstuff.DBStuff;
+import help.Help;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -14,20 +17,49 @@ public class Main {
 
         System.out.print("Hi, please type command or ? for help\n> ");
         Scanner userinput = new Scanner(System.in);
+        boolean allowed = false;
 
-        DBStuff mydb = new DBStuff("juja", "jujauser", "password");
-        mydb.connect();
-        try {
-            while (!mydb.connection.isClosed()) {
-                String question = userinput.nextLine();
-                System.out.print(mydb.getdbuser() + "@" + mydb.getdbname() + " > " + question);
-                if (question.equals("disconnect")) {
-                    mydb.connection.close();
+        while (true) {
+            String command = userinput.nextLine();
+            if (command.equals("exit")) {
+                break;
+            }
+            String[] tokens = command.split(" ");
+            DBStuff mydb = new DBStuff("juja", "jujauser", "password");
+
+            Method[] declaredMethods = DBStuff.class.getDeclaredMethods();
+            for (Method m : declaredMethods) {
+                if (m.getName().equals(tokens[0])) {
+                    allowed = true;
+                    try {
+                        m.invoke(mydb);
+
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-            System.out.print("\nConnection to DB was closed\n> ");
-        } catch (SQLException e) {
-            e.printStackTrace();
+            if (!allowed) {
+                System.out.println("Command not found!");
+                Help.commandList();
+                continue;
+            }
+            try {
+                while (!mydb.connection.isClosed()) {
+                    String question = userinput.nextLine();
+                    System.out.print(mydb.getdbuser() + "@" + mydb.getdbname() + " > " + question);
+                    if (question.equals("disconnect")) {
+                        mydb.connection.close();
+                    }
+                }
+                System.out.print("\nConnection to DB was closed\n> ");
+                allowed = false;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
