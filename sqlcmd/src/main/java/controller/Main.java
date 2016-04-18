629dccfb52bf5a5132c8291c3a5542b495e1b562
vadim <v@dim.com>
2016-04-18 23:31:24 +0300

    plus mockito

--- sqlcmd/src/main/java/controller/Main.java
+++ sqlcmd/src/main/java/controller/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         View view = new Cosole();
         DatabaseManager databaseManager = new MySQLDatabaseManager("", "");
 
@@ -19,4 +20,5 @@ public class Main {
         mainController.run();
 
     }
+
 }
