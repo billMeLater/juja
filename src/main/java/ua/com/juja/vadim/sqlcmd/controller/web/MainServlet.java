220dcaa961a2071f1432f746dcdcf741620f0d3d
billMeLater <v.kalitsev@gmail.com>
2017-11-02 20:58:00 +0200

    new day

--- src/main/java/ua/com/juja/vadim/sqlcmd/controller/web/MainServlet.java
+++ src/main/java/ua/com/juja/vadim/sqlcmd/controller/web/MainServlet.java
@@ -1,5 +1,7 @@
 package ua.com.juja.vadim.sqlcmd.controller.web;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import ua.com.juja.vadim.sqlcmd.controller.command.CommandOutput;
 import ua.com.juja.vadim.sqlcmd.model.DatabaseManager;
 import ua.com.juja.vadim.sqlcmd.model.MySQLDatabaseManager;
@@ -8,6 +10,7 @@ import ua.com.juja.vadim.sqlcmd.service.ServiceImpl;
 import ua.com.juja.vadim.sqlcmd.view.Console;
 import ua.com.juja.vadim.sqlcmd.view.View;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,8 +19,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class MainServlet extends HttpServlet {
     DatabaseManager databaseManager = new MySQLDatabaseManager();
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
