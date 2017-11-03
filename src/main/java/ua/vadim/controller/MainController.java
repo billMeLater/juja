cd1918bff45eea815f336d4130a209d215af112c
billMeLater <v.kalitsev@gmail.com>
2017-11-03 19:16:36 +0200

    new day

---
+++ src/main/java/ua/vadim/controller/MainController.java
@@ -0,0 +1,22 @@
+package ua.vadim.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ua.vadim.services.MessageService;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private MessageService service;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        String msg = "hi Bob";
+        model.addAttribute("msg", service.sentMessage(msg));
+        return "main";
+    }
+}
--- src/main/java/ua/vadim/services/Bob.java
+++ src/main/java/ua/vadim/services/Bob.java
@@ -2,7 +2,7 @@ package ua.vadim.services;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class Bob implements MessageService {
     public String sentMessage(String message) {
         return "Bob says: " + message;
